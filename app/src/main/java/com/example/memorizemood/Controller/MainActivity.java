db14a28da7787bedd3ed7fa7b9b1b35cdcc48e98
hack-shini <comenda.dimitri007@gmail.com>
2019-02-11 09:50:12 +0100

    Création méthode alterDialogComment qui affichera un alertDialog en appuyant sur ImageButton add_note

--- app/src/main/java/com/example/memorizemood/Controller/MainActivity.java
+++ app/src/main/java/com/example/memorizemood/Controller/MainActivity.java
@@ -1,20 +1,83 @@
 package com.example.memorizemood.Controller;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.memorizemood.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton addNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addNote = (ImageButton) findViewById(R.id.add_note_imageBtn);
 
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialogComment();
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+    public void alertDialogComment(){
+
+        // Modification layout -> View
         LayoutInflater factory = LayoutInflater.from(this);
+        final View alertDialogView = factory.inflate(R.layout.alert_dialog_comment_main, null);
 
+        // Creation of alertDialog
+        final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+
+        // Assigning custom view to alertDialog
+        adb.setView(alertDialogView);
+
+        // Title of alertDialog
+        adb.setTitle("Commentaire");
+
+        // Modification icon alertDialog
+        adb.setIcon(android.R.drawable.ic_dialog_info);
+
+        // Affected button "OK" to alertDialog and new event
+        adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // When we'll click on button "OK", we'll recover EditText corresponding to our custom view
+                EditText editText = (EditText)alertDialogView.findViewById(R.id.chose_comment_editText);
+
+            }
+        });
+
+        // When click on "Annuler" we go out of the alertDialog
+        adb.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        adb.show();
     }
 }
